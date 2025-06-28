db263dad477f5a3f7764c2a6028242364fb49f22
okyohe <yohey.watanave@gmail.com>
2025-06-28 11:33:58 +0900

    vite-ssgを再導入し、firebase hostingにデプロイ

--- src/entry.js
+++ src/entry.js
@@ -1,15 +1,8 @@
 // src/entry.js
-import { createApp } from "vue";
-import { createRouter, createWebHistory } from "vue-router";
+import { ViteSSG } from "vite-ssg";
 import App from "./App.vue";
 import "./assets/index.css";
 
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [{ path: "/", component: App, name: "home" }],
-});
+const routes = [{ path: "/", component: App, name: "home" }];
 
-const app = createApp(App);
-app.use(router);
-
-app.mount("#app");
+export const createApp = ViteSSG(App, { routes });
